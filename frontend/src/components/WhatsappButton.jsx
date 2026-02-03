3d5119fb0ac4366ac6ff9b4866d1089848cd5f63
lananxagst <67673253+lananxagst@users.noreply.github.com>
2026-02-03 14:00:46 +0800

    change whatsapp number for bot

--- frontend/src/components/WhatsappButton.jsx
+++ frontend/src/components/WhatsappButton.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/+6285190231484?text=Halo%20saya%20ingin%20bertanya%20tentang%20produk"
+      href="https://wa.me/+6282146448183?text=Halo%20saya%20ingin%20bertanya%20tentang%20produk"
       target="_blank"
       rel="noopener noreferrer"
       className="floating-whatsapp"
